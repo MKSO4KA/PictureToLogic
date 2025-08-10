4c0d70da043583b2fde674c16f954782b372ca32
MKSO4KA <mk06ru@gmail.com>
2025-08-10 12:44:20 +0300

    Build at 2025-08-10 12:44:19

--- src/com/mkso4ka/mindustry/matrixproc/PictureToLogicMod.java
+++ src/com/mkso4ka/mindustry/matrixproc/PictureToLogicMod.java
@@ -1,3 +1,4 @@
+// src/com/mkso4ka/mindustry/matrixproc/PictureToLogicMod.java
 package com.mkso4ka.mindustry.matrixproc;
 
 import mindustry.mod.*;
@@ -8,7 +9,17 @@ import arc.Events;
 public class PictureToLogicMod extends Mod {
 
     public PictureToLogicMod(){
+        // Этот конструктор вызывается при обнаружении мода.
+        // Оставляем его для логов или предварительных действий.
+        Log.info("PictureToLogicMod constructor called.");
+    }
+
+    @Override
+    public void init(){
+        // Метод init() вызывается после загрузки всех основных компонентов игры.
+        // Это самое правильное и безопасное место для создания и добавления UI элементов.
         Events.on(ClientLoadEvent.class, e -> {
+            // Запускаем построение нашего UI
             ModUI.build();
             Log.info("PictureToLogic UI Initialized.");
         });
@@ -17,5 +28,7 @@ public class PictureToLogicMod extends Mod {
     @Override
     public void loadContent(){
         Log.info("Loading PictureToLogic content...");
+        // Этот метод предназначен для загрузки контента (блоков, предметов и т.д.),
+        // для UI он не совсем подходит.
     }
 }
