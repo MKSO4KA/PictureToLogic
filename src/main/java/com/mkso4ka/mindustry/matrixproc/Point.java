ef68113ffa3d55eb065cab79ef19a1266926c4ac
MKSO4KA <mk06ru@gmail.com>
2025-08-12 13:45:28 +0300

    Build at 2025-08-12 13:45:28

---
+++ src/main/java/com/mkso4ka/mindustry/matrixproc/Point.java
@@ -0,0 +1,14 @@
+package com.mkso4ka.mindustry.matrixproc;
+
+/**
+ * Простой класс для хранения пары целочисленных координат (x, y).
+ */
+public class Point {
+    public final int x;
+    public final int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
