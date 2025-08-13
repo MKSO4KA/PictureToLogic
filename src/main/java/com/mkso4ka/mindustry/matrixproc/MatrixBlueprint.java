45efdc9853e73d9bbf79defbe7c6580999750c35
MKSO4KA <mk06ru@gmail.com>
2025-08-13 10:23:02 +0300

    Revert to state of commit a625c8c

--- src/main/java/com/mkso4ka/mindustry/matrixproc/MatrixBlueprint.java
+++ src/main/java/com/mkso4ka/mindustry/matrixproc/MatrixBlueprint.java
@@ -2,9 +2,8 @@ package com.mkso4ka.mindustry.matrixproc;
 
 import org.waveware.delaunator.DPoint;
 
-// Этот класс должен быть public и находиться в своем собственном файле.
 public class MatrixBlueprint {
-    final int n, m; // Ширина и высота итоговой матрицы в тайлах
+    final int n, m; // Ширина (n) и высота (m) итоговой матрицы в тайлах
     final DPoint[] displayBottomLefts;
 
     MatrixBlueprint(int n, int m, DPoint[] displayBottomLefts) {
